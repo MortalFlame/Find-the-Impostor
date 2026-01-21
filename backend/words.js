49ed1642eb777de20c26304f31ff67a495a4f7cc
MortalFlame <ahmadayub1@yahoo.com>
2026-01-21 20:27:47 +0400

    Create words.js

---
+++ backend/words.js
@@ -0,0 +1,7 @@
+[
+  { "word": "apple", "hint": "fruit" },
+  { "word": "dog", "hint": "pet" },
+  { "word": "car", "hint": "vehicle" },
+  { "word": "sun", "hint": "star" },
+  { "word": "piano", "hint": "instrument" }
+]
